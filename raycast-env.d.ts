07e3c56524ec47ef1db8f2b067d812f4796d65d2
Anh Thang Bui <buianhthang89@gmail.com>
2023-09-11 17:11:05 +0700

    feat: update new pokemons, upgrade deps

---
+++ raycast-env.d.ts
@@ -0,0 +1,44 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** Language - Your preferred language. Supporting Pokémon command only. */
+  "language": "12" | "4" | "9" | "5" | "6" | "8" | "1" | "3" | "7"
+}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `pokemon` command */
+  export type Pokemon = ExtensionPreferences & {}
+  /** Preferences accessible in the `ability` command */
+  export type Ability = ExtensionPreferences & {}
+  /** Preferences accessible in the `move` command */
+  export type Move = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `pokemon` command */
+  export type Pokemon = {}
+  /** Arguments passed to the `ability` command */
+  export type Ability = {}
+  /** Arguments passed to the `move` command */
+  export type Move = {}
+}
+
+
+declare module "swift:*" {
+  function run<T = unknown, U = any>(command: string, input?: U): Promise<T>;
+  export default run;
+	export class SwiftError extends Error {
+    stderr: string;
+    stdout: string;
+  }
+}
