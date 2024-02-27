76cb1cc6e38a3683362f66120e30c8d1cd60d348
Anh Thang Bui <buianhthang89@gmail.com>
2024-02-27 14:23:10 +0700

    fix: Pokédex up to date

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -33,12 +33,3 @@ declare namespace Arguments {
   export type Move = {}
 }
 
-
-declare module "swift:*" {
-  function run<T = unknown, U = any>(command: string, input?: U): Promise<T>;
-  export default run;
-	export class SwiftError extends Error {
-    stderr: string;
-    stdout: string;
-  }
-}
